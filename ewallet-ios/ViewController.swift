1bf2488421d2c529d6bda68ddfa7df433623d32d
Phan Quang Hoang <Hoang.PhanQuang@nashtechglobal.com>
2024-01-25 00:16:51 +0700

    Init project

---
+++ ewallet-ios/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ewallet-ios
+//
+//  Created by hoang phan on 24/01/2024.
+//
+
+import UIKit
+import ElectrodeContainer
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let miniapp = ElectrodeReactNative.sharedInstance().miniApp(withName: "MainMiniapp", properties: nil, overlay: false, sizeFlexibility: 0, delegate: nil)
+        miniapp.view.frame = UIScreen.main.bounds
+        view.addSubview(miniapp.view)
+    }
+
+
+}
+
