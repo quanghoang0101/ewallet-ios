e6ce85158234f1b1a70a2efe8c0edba5c1bfac4b
Phan Quang Hoang <Hoang.PhanQuang@nashtechglobal.com>
2024-01-28 13:57:38 +0700

    Update DashboardTabBarController.swift

--- ewallet-ios/Presentations/DashboardTabBarController.swift
+++ ewallet-ios/Presentations/DashboardTabBarController.swift
@@ -17,19 +17,19 @@ class DashboardTabBarController: UITabBarController, UITabBarControllerDelegate 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let home = BaseNavigationController(rootViewController: HomeViewController())
-        let homeIc = UITabBarItem(title: "", image: UIImage(named: "ic-home.png"), selectedImage: UIImage(named: "ic-home.png"))
+        let homeIc = UITabBarItem(title: "Home", image: UIImage(named: "ic-home.png"), selectedImage: UIImage(named: "ic-home.png"))
         home.tabBarItem = homeIc
         
         let wallet = BaseNavigationController(rootViewController: WalletViewController())
-        let walletIc = UITabBarItem(title: "", image: UIImage(named: "ic-wallet.png"), selectedImage: UIImage(named: "ic-wallet.png"))
+        let walletIc = UITabBarItem(title: "Wallet", image: UIImage(named: "ic-wallet.png"), selectedImage: UIImage(named: "ic-wallet.png"))
         wallet.tabBarItem = walletIc
         
         let statistics = BaseNavigationController(rootViewController: StatisticsViewController())
-        let statisticsIc = UITabBarItem(title: "", image: UIImage(named: "ic-statistics.png"), selectedImage: UIImage(named: "ic-statistics.png"))
+        let statisticsIc = UITabBarItem(title: "Statistics", image: UIImage(named: "ic-statistics.png"), selectedImage: UIImage(named: "ic-statistics.png"))
         statistics.tabBarItem = statisticsIc
         
         let voucher = BaseNavigationController(rootViewController: VoucherViewController())
-        let voucherIc = UITabBarItem(title: "", image: UIImage(named: "ic-voucher.png"), selectedImage: UIImage(named: "ic-voucher.png"))
+        let voucherIc = UITabBarItem(title: "Voucher", image: UIImage(named: "ic-voucher.png"), selectedImage: UIImage(named: "ic-voucher.png"))
         voucher.tabBarItem = voucherIc
         
         let controllers = [home, wallet, statistics, voucher]
